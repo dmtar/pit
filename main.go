32543bda1b60364da23037ff5adece62ca6e3931
Nedyalko Dyakov <me@ndyakov.eu>
2015-01-29 23:11:37 +0200

    Remove hello function from main.go

--- main.go
+++ main.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/zenazn/goji"
-	"github.com/zenazn/goji/web"
 
 	"github.com/dmtar/pit/controllers"
 	"github.com/dmtar/pit/middlewares"
 )
-
-func hello(c web.C, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", c.URLParams["name"])
-}
 
 func init() {
 	goji.Use(middlewares.JSON)
