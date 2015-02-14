39f54daaf10ec411c51fdd7c758e174b3ac120ac
Nedyalko Dyakov <me@ndyakov.eu>
2015-02-14 12:32:10 +0200

    Add indexes.

--- models/location.go
+++ models/location.go
@@ -1,6 +1,11 @@
 package models
 
-import "strconv"
+import (
+	"fmt"
+	"strconv"
+
+	"gopkg.in/mgo.v2/bson"
+)
 
 type Location struct {
 	Longitude float64 `bson:"lng" json:"lng"`
@@ -24,4 +29,47 @@ func ParseFloat64(input string) float64 {
 	}
 
 	return result
+}
+
+// GetBSON is actually implementing the bson.Getter
+// interface so this can be Marshaled into bson.
+func (l Location) GetBSON() (interface{}, error) {
+	res := bson.M{
+		"name":        l.Name,
+		"type":        "Point",
+		"coordinates": []float64{l.Longitude, l.Latitude},
+	}
+	return res, nil
+}
+
+// SetBSON is implementing the bson.Setter interface
+// so this can be Unmarshaled from bson.
+// To implement the Setter interface we need to import
+// the bson package, so we can have the bson.Raw type available.
+// This one may panic with index out of range. We couldn't care less
+// for that at the moment.
+func (l *Location) SetBSON(raw bson.Raw) error {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("\n\n!!!!! PANIC !!!!!! %s ! \n\n", r)
+		}
+	}()
+
+	response := struct {
+		Name        string    `bson:"name"`
+		Type        string    `bson:"type"`
+		Coordinates []float64 `bson:"coordinates"`
+	}{}
+
+	err := raw.Unmarshal(&response)
+
+	if err != nil {
+		return err
+	}
+
+	l.Name = response.Name
+	l.Longitude = response.Coordinates[0]
+	l.Latitude = response.Coordinates[1]
+
+	return nil
 }
--- system/application.go
+++ system/application.go
@@ -1,15 +1,113 @@
 package system
 
 import (
+	"fmt"
+
 	"github.com/dmtar/pit/config"
 	"github.com/gorilla/sessions"
+	"gopkg.in/mgo.v2"
 )
 
 type Application struct {
-	Store *sessions.CookieStore
+	Store   *sessions.CookieStore
+	db      *mgo.Database
+	session *mgo.Session
 }
 
 func (application *Application) Init() {
 	secret := []byte(config.Secret)
 	application.Store = sessions.NewCookieStore(secret)
+	go application.CreateIndexes()
+}
+
+func (application *Application) CreateIndexes() {
+	var err error
+	application.session, err = mgo.Dial("localhost")
+	if err != nil {
+		check(fmt.Errorf("Can't connect to mongo, go error: %v", err))
+	}
+	application.db = application.session.DB("pit")
+
+	application.createIndexesOnPictures()
+	application.createIndexesOnAlbums()
+	application.createIndexesOnNotifications()
+
+}
+
+func (application *Application) createIndexesOnPictures() {
+	var err error
+	collection := application.db.C("fs.files")
+	err = collection.EnsureIndex(mgo.Index{
+		Key:        []string{"$2dsphere:metadata.location"},
+		Background: true,
+	})
+	check(err)
+
+	err = collection.EnsureIndex(mgo.Index{
+		Key:        []string{"metadata.tags"},
+		Background: true,
+	})
+	check(err)
+
+	err = collection.EnsureIndex(mgo.Index{
+		Key:        []string{"metadata.album"},
+		Background: true,
+	})
+	check(err)
+
+	err = collection.EnsureIndex(mgo.Index{
+		Key:        []string{"metadata.user"},
+		Background: true,
+	})
+	check(err)
+}
+
+func (application *Application) createIndexesOnAlbums() {
+	var err error
+	collection := application.db.C("albums")
+	err = collection.EnsureIndex(mgo.Index{
+		Key:        []string{"$2dsphere:location"},
+		Background: true,
+	})
+	check(err)
+
+	err = collection.EnsureIndex(mgo.Index{
+		Key:        []string{"metadata.tags"},
+		Background: true,
+	})
+	check(err)
+
+	err = collection.EnsureIndex(mgo.Index{
+		Key:        []string{"data_range.start"},
+		Background: true,
+	})
+	check(err)
+
+	err = collection.EnsureIndex(mgo.Index{
+		Key:        []string{"data_range.end"},
+		Background: true,
+	})
+	check(err)
+
+	err = collection.EnsureIndex(mgo.Index{
+		Key:        []string{"user"},
+		Background: true,
+	})
+	check(err)
+}
+
+func (application *Application) createIndexesOnNotifications() {
+	var err error
+	collection := application.db.C("notifications")
+	err = collection.EnsureIndex(mgo.Index{
+		Key:        []string{"user"},
+		Background: true,
+	})
+	check(err)
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
